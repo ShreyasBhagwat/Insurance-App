554304525921247cd32daf10653b2253049c2fbd
Dhruvkumar-patel98 <pateldhruv9726@gmail.com>
2023-02-05 22:13:40 +0530

    Remove file

--- app/src/main/java/com/example/insurance_app/UserDashboardActivity.kt
+++ app/src/main/java/com/example/insurance_app/UserDashboardActivity.kt
@@ -2,7 +2,6 @@ package com.example.insurance_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.annotation.SuppressLint
 import androidx.fragment.app.Fragment
 import com.example.insurance_app.ViewModels.Help
 import com.example.insurance_app.ViewModels.Home
@@ -16,7 +15,6 @@ class UserDashboardActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityUserDashboardBinding
 
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityUserDashboardBinding.inflate(layoutInflater)
