f4e0e767c4dbf0bbb80a5def4efdc4f87817860f
Dhruvkumar-patel98 <pateldhruv9726@gmail.com>
2023-02-07 15:05:48 +0530

    Splach screen added

--- app/src/main/java/com/example/insurance_app/SplashScreenActivity.kt
+++ app/src/main/java/com/example/insurance_app/SplashScreenActivity.kt
@@ -1,11 +1,20 @@
 package com.example.insurance_app
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        val handler = Handler()
+        handler.postDelayed(Runnable { // TODO: Your application init goes here.
+            val mInHome = Intent(this, LoginActivity::class.java)
+            this.startActivity(mInHome)
+            this.finish()
+        }, 3000)
     }
 }
