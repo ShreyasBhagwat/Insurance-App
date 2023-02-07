93e75c14e66203d89e184f0728231ba142bd180f
Dhruvkumar-patel98 <pateldhruv9726@gmail.com>
2023-02-07 17:04:05 +0530

    Final working

--- app/src/main/java/com/example/insurance_app/ViewModels/PolicyFragment.kt
+++ app/src/main/java/com/example/insurance_app/ViewModels/PolicyFragment.kt
@@ -1,11 +1,20 @@
 package com.example.insurance_app.ViewModels
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import com.example.insurance_app.EnterDetails
 import com.example.insurance_app.R
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +30,9 @@ class PolicyFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    lateinit var mainLinerLayoutForRegistrationNumber: LinearLayout
+    lateinit var sharedPreferences: SharedPreferences
+    lateinit var sharedPreferencesEditor: SharedPreferences.Editor
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,26 +47,76 @@ class PolicyFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_policy, container, false)
+        val view=inflater.inflate(R.layout.fragment_policy, container, false)
+        val carbtn: CardView =view.findViewById(R.id.carCardView)
+        carbtn.setOnClickListener{
+            val btn:String="Car"
+            val view = layoutInflater.inflate(R.layout.registration_number_bottom_sheet_dialog, null)
+            mainLinerLayoutForRegistrationNumber=view.findViewById(R.id.mainLinerLayoutForResistrationNumber)
+            mainLinerLayoutForRegistrationNumber.background=resources.getDrawable(R.drawable.background_car,null)
+            openRegistrationNumberBottomScreen(btn,view)
+        }
+        val bikebtn: CardView =view.findViewById(R.id.bikeCardView)
+        bikebtn.setOnClickListener{
+            val btn:String="Bike"
+            val view = layoutInflater.inflate(R.layout.registration_number_bottom_sheet_dialog, null)
+            mainLinerLayoutForRegistrationNumber=view.findViewById(R.id.mainLinerLayoutForResistrationNumber)
+            mainLinerLayoutForRegistrationNumber.background=resources.getDrawable(R.drawable.background_bike,null)
+            openRegistrationNumberBottomScreen(btn, view)
+        }
+        val mobilebtn: CardView =view.findViewById(R.id.mobileCardView)
+        mobilebtn.setOnClickListener{
+            val btn:String="Mobile"
+            val view = layoutInflater.inflate(R.layout.registration_number_bottom_sheet_dialog, null)
+            mainLinerLayoutForRegistrationNumber=view.findViewById(R.id.mainLinerLayoutForResistrationNumber)
+            mainLinerLayoutForRegistrationNumber.background=resources.getDrawable(R.drawable.background_mobile,null)
+            openRegistrationNumberBottomScreen(btn, view)
+        }
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Policy.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PolicyFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun openRegistrationNumberBottomScreen(btn: String, view: View) {
+        val dialog = BottomSheetDialog(context!!)
+
+        val registrationNumber: EditText =view.findViewById(R.id.vehicleRegistrationNumber)
+        mainLinerLayoutForRegistrationNumber=view.findViewById(R.id.mainLinerLayoutForResistrationNumber)
+        val title: TextView =view.findViewById(R.id.titleForBottomSheet)
+        if(btn=="Car"){
+            title.text="Get up to 40% off on car insurance"
+        }
+        if(btn=="Bike"){
+            title.text="Bike insurance starting at ₹555"
+        }
+        if(btn=="Mobile"){
+            title.text="Save mobile at just ₹200"
+            registrationNumber.setHint("Enter previous insurance ID")
+        }
+        val goToEnterUserPolicyDetailsBtn = view.findViewById<ImageButton>(R.id.goToEnterUserPolicyDetails)
+        val newInsuranceBtn=view.findViewById<Button>(R.id.newInsuranceBtn)
+        goToEnterUserPolicyDetailsBtn.setOnClickListener {
+            if(TextUtils.isEmpty(registrationNumber.text.toString())){
+                Toast.makeText(context, "Enter Vehicle Number", Toast.LENGTH_SHORT).show()
             }
+            else {
+                sharedPreferences = this.activity!!.getSharedPreferences(
+                    "numberPlate",
+                    AppCompatActivity.MODE_PRIVATE
+                )
+                sharedPreferencesEditor = sharedPreferences.edit()
+                sharedPreferencesEditor.putString("number", registrationNumber.text.toString())
+                sharedPreferencesEditor.apply()
+                val intent: Intent = Intent(context, EnterDetails::class.java)
+                intent.putExtra("Vehicle",btn)
+                startActivity(intent)
+            }
+        }
+        newInsuranceBtn.setOnClickListener{
+            val intent: Intent = Intent(context, EnterDetails::class.java)
+            intent.putExtra("Vehicle",btn)
+            startActivity(intent)
+        }
+        dialog.setCancelable(true)
+        dialog.setContentView(view)
+        dialog.show()
     }
 }
